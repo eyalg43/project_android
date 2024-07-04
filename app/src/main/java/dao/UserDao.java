4dfc40e60bc77df1baf9928dd3b3481a27c41bde
Omri Cohen <omricohen19@gmail.com>
2024-07-04 22:19:48 +0300

    added needed files

---
+++ app/src/main/java/dao/UserDao.java
@@ -0,0 +1,36 @@
+package dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.project_android.entities.User;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+    // Insert a single user
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertUser(User user);
+
+    // Update a user
+    @Update
+    void updateUser(User user);
+
+    // Delete a user
+    @Delete
+    void deleteUser(User user);
+
+    // Fetch a user by ID
+    @Query("SELECT * FROM user WHERE id = :userId")
+    LiveData<User> getUserById(String userId);
+
+    // Fetch all users
+    @Query("SELECT * FROM user")
+    LiveData<List<User>> getAllUsers();
+}
